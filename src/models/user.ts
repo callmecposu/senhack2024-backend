d24cede9cf5c5498367d1a61b94184b15e4a35a8
Vlad Huziienko <guzienko.vlad@gmail.com>
2024-03-09 13:46:14 -0500

    schemas added

---
+++ src/models/user.ts
@@ -0,0 +1,46 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+interface UserInfo {
+    interests: string[];
+    nativeLanguages: string[];
+    otherLanguages: string[];
+    personalityType: string;
+}
+
+interface User extends Document {
+    fName: string;
+    lName: string;
+    anonName: string;
+    email: string;
+    password: string;
+    bio: string;
+    aboutUser: UserInfo;
+    roomatePreferences: UserInfo;
+    blockList: string[];
+    notInterested: string[];
+}
+
+const userSchema: Schema = new Schema({
+    fName: { type: String, required: true },
+    lName: { type: String, required: true },
+    anonName: { type: String, default: '' },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    bio: { type: String, default: '' },
+    aboutUser: {
+        interests: { type: [String], default: [] },
+        nativeLanguages: { type: [String], default: [] },
+        otherLanguages: { type: [String], default: [] },
+        personalityType: { type: String, default: '' },
+    },
+    roomatePreferences: {
+        interests: { type: [String], default: [] },
+        nativeLanguages: { type: [String], default: [] },
+        otherLanguages: { type: [String], default: [] },
+        personalityType: { type: String, default: '' },
+    },
+    blockList: { type: [String], default: [] },
+    notInterested: { type: [String], default: [] },
+});
+
+export default mongoose.model<User>('User', userSchema);
