3498222e5f72c6820c2a2e6d3823b03c4cd431d3
callmecposu <pc.cposu@gmail.com>
2024-03-09 20:59:18 -0500

    added event notification on createChat

--- src/implementations/chat/createChat.ts
+++ src/implementations/chat/createChat.ts
@@ -2,6 +2,7 @@ import { ServerUnaryCall, sendUnaryData, status } from "@grpc/grpc-js";
 import * as ch_pb from "../../protos/out/chat/chat_pb";
 import * as ev_pb from "../../protos/out/events/events_pb";
 import ChatModel from "../../models/chat";
+import { getEventStreams } from "../events/eventStreams";
 
 const createChat = async (
     call: ServerUnaryCall<ch_pb.CreateChatRequest, ch_pb.Chat>,
@@ -23,7 +24,7 @@ const createChat = async (
         // create a new chat
         const newChat = new ChatModel({ users: req.getUsersList() });
         const savedChat = await newChat.save();
-        // create the response
+        // create the response chat
         const respChat = new ch_pb.Chat();
         respChat.setId(savedChat._id.toString());
         savedChat.users.forEach((user) => {
@@ -31,6 +32,18 @@ const createChat = async (
         });
         respChat.setDateCreated(savedChat.dateCreated.toString());
         respChat.setRevealIdentityList([]);
+        // create the new chat event
+        const ev = new ev_pb.Event()
+        const newChatEv = new ev_pb.NewChatEvent()
+        newChatEv.setChat(respChat)
+        ev.setNewChatEvent(newChatEv)
+        // send the new chat event to the users
+        req.getUsersList().forEach(userID => {
+            const userEventStream = getEventStreams().filter(es => es.userID == userID)
+            if (userEventStream.length != 0){
+                userEventStream[0].stream.write(ev)
+            }
+        })
         // send the response
         callback(null, respChat);
     } catch (err: any) {
